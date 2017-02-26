c9777ab750aaae17d15a574c19e6c2e43f7b8600
cqzan <363166818@qq.com>
2017-02-26 21:55:33 +0800

    Update tieba.py

--- tieba.py
+++ tieba.py
@@ -5,8 +5,8 @@ import urllib
 import time
 import multiprocessing
 headers={
-    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36",
-    "Cookie":"TIEBA_USERTYPE=f5b3af10c72414574c46106f; bdshare_firstime=1469268550290; TIEBAUID=9c0c5e238b095b0c4c1efa61; rpln_guide=1; BDUSS=GJzbU0tNnJOY0toM0xXTFJsSjFrMER0NHRuZEMxRFFWQjJaTFZKbDhlZExHZXBYQVFBQUFBJCQAAAAAAAAAAAEAAAChfcIRs~7stsu5AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEuMwldLjMJXb; Hm_lvt_287705c8d9e2073d13275b18dbd746dc=1474630150,1474631449,1474776888; BAIDUID=D9ADA052E8F2D21C5FF0C469536DC5AE:FG=1; PSTM=1477225027; BIDUPSID=1FDE40D4D51192045C644DDD902D2677; __cfduid=d7b69e4ac28c7535b69b645bd7a8b94311481117304; STOKEN=81785a1a09b324efed40b602d990cf8e94fc5c1184ef9fc46dcda31bd2599760; wise_device=0; PSINO=1; H_PS_PSSID=1440_21108_21312_22035_22025; LONGID=297958817"
+    "User-Agent":"",
+    "Cookie":""
 }
 
 def get_url(url_tieba,n):
@@ -54,7 +54,7 @@ def main(urls):
         pool.apply_async(geren_pic,(url,))
     pool.close()
     pool.join()
-
+    #获取cpu数，多进程爬取
 if __name__=="__main__":
     url_tieba="http://tieba.baidu.com/f/like/furank?kw=%D3%A2%D0%DB%D6%BE&pn={}"
     n=21
